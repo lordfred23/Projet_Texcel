aca8bbfadea2f9d2c25b89af68c95d608638a7fd
Alex_SauceBrune <saucebrunee@gmail.com>
2017-02-21 21:03:30 -0500

    Renommer Interface - Code

--- Projet_Texcel/Projet_Texcel/Jeu.Designer.cs
+++ Projet_Texcel/Projet_Texcel/Jeu.Designer.cs
@@ -30,20 +30,20 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
+            this.txtTheme = new System.Windows.Forms.TextBox();
+            this.txtClass = new System.Windows.Forms.TextBox();
+            this.txtGenre = new System.Windows.Forms.TextBox();
+            this.txtDev = new System.Windows.Forms.TextBox();
+            this.txtNom = new System.Windows.Forms.TextBox();
+            this.txtConfig = new System.Windows.Forms.RichTextBox();
+            this.txtDesc = new System.Windows.Forms.RichTextBox();
             this.label7 = new System.Windows.Forms.Label();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -58,13 +58,13 @@
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.textBox4);
-            this.groupBox1.Controls.Add(this.textBox5);
-            this.groupBox1.Controls.Add(this.textBox6);
-            this.groupBox1.Controls.Add(this.textBox2);
-            this.groupBox1.Controls.Add(this.textBox1);
-            this.groupBox1.Controls.Add(this.richTextBox2);
-            this.groupBox1.Controls.Add(this.richTextBox1);
+            this.groupBox1.Controls.Add(this.txtTheme);
+            this.groupBox1.Controls.Add(this.txtClass);
+            this.groupBox1.Controls.Add(this.txtGenre);
+            this.groupBox1.Controls.Add(this.txtDev);
+            this.groupBox1.Controls.Add(this.txtNom);
+            this.groupBox1.Controls.Add(this.txtConfig);
+            this.groupBox1.Controls.Add(this.txtDesc);
             this.groupBox1.Controls.Add(this.label7);
             this.groupBox1.Controls.Add(this.label6);
             this.groupBox1.Controls.Add(this.label5);
@@ -79,59 +79,56 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Jeu";
             // 
-            // label1
+            // txtTheme
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(7, 22);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(45, 17);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Nom :";
+            this.txtTheme.Location = new System.Drawing.Point(120, 317);
+            this.txtTheme.Name = "txtTheme";
+            this.txtTheme.Size = new System.Drawing.Size(147, 22);
+            this.txtTheme.TabIndex = 14;
             // 
-            // label2
+            // txtClass
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(7, 81);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(87, 17);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Description :";
+            this.txtClass.Location = new System.Drawing.Point(120, 290);
+            this.txtClass.Name = "txtClass";
+            this.txtClass.Size = new System.Drawing.Size(147, 22);
+            this.txtClass.TabIndex = 13;
             // 
-            // label3
+            // txtGenre
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(7, 51);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(97, 17);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "Développeur :";
+            this.txtGenre.Location = new System.Drawing.Point(120, 262);
+            this.txtGenre.Name = "txtGenre";
+            this.txtGenre.Size = new System.Drawing.Size(147, 22);
+            this.txtGenre.TabIndex = 12;
             // 
-            // label4
+            // txtDev
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(7, 167);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(159, 17);
-            this.label4.TabIndex = 3;
-            this.label4.Text = "Configuration minimale :";
+            this.txtDev.Location = new System.Drawing.Point(120, 51);
+            this.txtDev.Name = "txtDev";
+            this.txtDev.Size = new System.Drawing.Size(147, 22);
+            this.txtDev.TabIndex = 10;
             // 
-            // label5
+            // txtNom
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(7, 266);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(56, 17);
-            this.label5.TabIndex = 4;
-            this.label5.Text = "Genre :";
+            this.txtNom.Location = new System.Drawing.Point(120, 21);
+            this.txtNom.Name = "txtNom";
+            this.txtNom.Size = new System.Drawing.Size(147, 22);
+            this.txtNom.TabIndex = 9;
             // 
-            // label6
+            // txtConfig
             // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(7, 294);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(98, 17);
-            this.label6.TabIndex = 5;
-            this.label6.Text = "Classification :";
+            this.txtConfig.Location = new System.Drawing.Point(10, 187);
+            this.txtConfig.Name = "txtConfig";
+            this.txtConfig.Size = new System.Drawing.Size(257, 62);
+            this.txtConfig.TabIndex = 8;
+            this.txtConfig.Text = "";
+            // 
+            // txtDesc
+            // 
+            this.txtDesc.Location = new System.Drawing.Point(10, 102);
+            this.txtDesc.Name = "txtDesc";
+            this.txtDesc.Size = new System.Drawing.Size(257, 62);
+            this.txtDesc.TabIndex = 7;
+            this.txtDesc.Text = "";
             // 
             // label7
             // 
@@ -142,56 +139,59 @@
             this.label7.TabIndex = 6;
             this.label7.Text = "Thème :";
             // 
-            // richTextBox1
+            // label6
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(10, 102);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(257, 62);
-            this.richTextBox1.TabIndex = 7;
-            this.richTextBox1.Text = "";
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(7, 294);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(98, 17);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Classification :";
             // 
-            // richTextBox2
+            // label5
             // 
-            this.richTextBox2.Location = new System.Drawing.Point(10, 187);
-            this.richTextBox2.Name = "richTextBox2";
-            this.richTextBox2.Size = new System.Drawing.Size(257, 62);
-            this.richTextBox2.TabIndex = 8;
-            this.richTextBox2.Text = "";
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(7, 266);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 17);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Genre :";
             // 
-            // textBox1
+            // label4
             // 
-            this.textBox1.Location = new System.Drawing.Point(120, 21);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(147, 22);
-            this.textBox1.TabIndex = 9;
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(7, 167);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(159, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Configuration minimale :";
             // 
-            // textBox2
+            // label3
             // 
-            this.textBox2.Location = new System.Drawing.Point(120, 51);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(147, 22);
-            this.textBox2.TabIndex = 10;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 51);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Développeur :";
             // 
-            // textBox4
+            // label2
             // 
-            this.textBox4.Location = new System.Drawing.Point(120, 317);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(147, 22);
-            this.textBox4.TabIndex = 14;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Description :";
             // 
-            // textBox5
+            // label1
             // 
-            this.textBox5.Location = new System.Drawing.Point(120, 290);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(147, 22);
-            this.textBox5.TabIndex = 13;
-            // 
-            // textBox6
-            // 
-            this.textBox6.Location = new System.Drawing.Point(120, 262);
-            this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(147, 22);
-            this.textBox6.TabIndex = 12;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nom :";
             // 
             // Jeu
             // 
@@ -213,13 +213,13 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox5;
-        private System.Windows.Forms.TextBox textBox6;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.RichTextBox richTextBox2;
-        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox txtTheme;
+        private System.Windows.Forms.TextBox txtClass;
+        private System.Windows.Forms.TextBox txtGenre;
+        private System.Windows.Forms.TextBox txtDev;
+        private System.Windows.Forms.TextBox txtNom;
+        private System.Windows.Forms.RichTextBox txtConfig;
+        private System.Windows.Forms.RichTextBox txtDesc;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label5;
