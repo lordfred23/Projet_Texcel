2ec5f32f9b10a134122eb80052e7a1aaf5f6eabf
lordfred23 <fred.cote23@gmail.com>
2017-04-24 08:27:38 -0400

    projet WEB

---
+++ ProjetTexcel-WEB/ProjetTexcel-WEB/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProjetTexcel_WEB.MvcApplication" Language="C#" %>
---
+++ ProjetTexcel-WEB/ProjetTexcel-WEB/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjetTexcel_WEB.Startup))]
+namespace ProjetTexcel_WEB
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
